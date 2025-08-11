af45e316529cf36182d1c80783c90c2fa145c88c
olga <koa0308@gmail.com>
2025-08-11 21:29:36 +0400

    .flake8

---
+++ src/masks.py
@@ -0,0 +1,19 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция принимает на вход номер карты в виде
+    числа и возвращает маску номера по правилу
+    XXXX XX** **** XXXX."""
+    card_number_str = str(card_number)
+    if len(card_number_str) == 16:
+        mask_number = f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[12:]}"
+        return mask_number
+    return "Введен неверный номер карты"
+
+
+def get_mask_account(account: int) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску.
+    Номер счета замаскирован и отображается в формате **XXXX"""
+    account_str = str(account)
+    if len(account_str) >= 4:
+        mask_account = f"** {account_str[-4:]}"
+        return mask_account
+    return "Введен неверный номер счета"
