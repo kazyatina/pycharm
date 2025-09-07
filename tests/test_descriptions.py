a3bcb769b58b818fba76456e82833e1fb18e7148
olga <koa0308@gmail.com>
2025-09-07 20:21:24 +0400

    тесты на функцию transaction_descriptions

---
+++ tests/test_descriptions.py
@@ -0,0 +1,27 @@
+import pytest
+from src.generators import transaction_descriptions
+
+@pytest.fixture
+def transactions():
+    return [
+        {"id": 1, "description": "Перевод организации"},
+        {"id": 2, "description": "Перевод с карты на счет"},
+    ]
+
+@pytest.mark.parametrize("expected", [
+    ["Перевод организации", "Перевод с карты на счет"],
+])
+def test_transaction_descriptions(transactions, expected):
+    descriptions = list(transaction_descriptions(transactions))
+    assert descriptions == expected
+
+@pytest.fixture
+def empty_descriptions():
+    return []
+
+@pytest.mark.parametrize("expected", [
+    ["Перевод организации", "Перевод с карты на счет"],
+])
+def test_empty_descriptions(empty_descriptions, expected):
+    empty_list = list(transaction_descriptions(empty_descriptions))
+    assert empty_list == ['Описания транзакций нет.']
